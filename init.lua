fc342e1ee5390ab7a26bcf4ef31a8a38e83768a1
rawrrawrpurpledinosaur <191292l@student.hci.edu.sg>
2024-09-01 17:39:37 +0800

    Initial LazyVim config

---
+++ init.lua
@@ -0,0 +1,19 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+-- Ensure this code is executed after luasnip is loaded
+local ls = require('luasnip')
+
+-- Unmap the Tab key in insert mode if it's mapped by luasnip
+vim.api.nvim_del_keymap('i', '<Tab>')
+
+
+-- Define the handle_tab function to handle what happens when Tab is pressed
+function _G.handle_tab()
+  if vim.fn.pumvisible() == 1 then
+    return "<C-n>"
+  else
+    return "<Tab>"
+  end
+end
+
---
+++ lua/config/keymaps.lua
@@ -0,0 +1,21 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local opts = { noremap = true, silent = true }
+
+-- Overrdide tab keymap and use it for copilot completion
+vim.api.nvim_set_keymap('i', '<Tab>', 'copilot#Accept("<C-J>")', { expr = true})
+
+-- Return to normal mode after jj or kk or jk
+vim.api.nvim_set_keymap('i', 'jj', '<Esc>', opts)
+vim.api.nvim_set_keymap('i', 'kk', '<Esc>', opts)
+vim.api.nvim_set_keymap('i', 'jk', '<Esc>', opts)
+
+-- Select all
+vim.api.nvim_set_keymap('n', '<C-a>', 'ggVG', opts)
+
+-- Save buffer 
+vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', opts)
+
+
